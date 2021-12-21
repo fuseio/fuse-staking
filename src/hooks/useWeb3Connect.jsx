5eb6687e347a471bfaf2013004e738b66dadac57
t0mcr8se <husamirami@gmail.com>
2021-12-21 19:02:59 +0300

    add walletlink support

--- src/hooks/useWeb3Connect.jsx
+++ src/hooks/useWeb3Connect.jsx
@@ -1,19 +1,42 @@
 import { useState } from 'react'
 import Web3Modal from 'web3modal'
 import WalletConnectProvider from '@fuseio/walletconnect-web3-provider'
+import WalletLink from 'walletlink'
 
 const providerOptions = {
-  metamask: {
-  },
+  metamask: {},
   walletconnect: {
     package: WalletConnectProvider,
     options: {
       bridge: 'https://walletconnect.fuse.io',
       rpc: {
-        122: 'https://rpc.fuse.io'
-      }
-    }
-  }
+        122: 'https://rpc.fuse.io',
+      },
+    },
+  },
+  'custom-walletlink': {
+    display: {
+      logo:
+        'https://raw.githubusercontent.com/walletlink/walletlink/master/web/src/images/wallets/coinbase-wallet.svg',
+      name: 'WalletLink',
+      description: 'Scan with WalletLink to connect',
+    },
+    options: {
+      appName: 'Fuse Staking',
+      networkUrl: `https://rpc.fuse.io`,
+      chainId: 122,
+    },
+    package: WalletLink,
+    connector: async (_, options) => {
+      const { appName, networkUrl, chainId } = options
+      const walletLink = new WalletLink({
+        appName,
+      })
+      const provider = walletLink.makeWeb3Provider(networkUrl, chainId)
+      await provider.enable()
+      return provider
+    },
+  },
 }
 
 const useWeb3Connect = (connectCallback) => {
