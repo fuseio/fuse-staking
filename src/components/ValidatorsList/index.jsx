875fa696edca349312ed4b7c4063dea14831eace
Mayank Mittal <msquare.pvt21@gmail.com>
2023-03-10 23:47:57 +0530

    [FIX] Moved images away from api

--- src/components/ValidatorsList/index.jsx
+++ src/components/ValidatorsList/index.jsx
@@ -1,128 +1,223 @@
-import React, { useMemo } from 'react'
-import { isMobile } from 'react-device-detect'
-import { useSelector, useDispatch } from 'react-redux'
-import { Formik, Form, useFormikContext } from 'formik'
-import { object, string, boolean } from 'yup'
-import map from 'lodash/map'
-import filter from 'lodash/filter'
-import sortBy from 'lodash/sortBy'
-import Table from '@/components/common/Table'
-import TableHeader from '@/components/common/Table/TableHeader'
-import TableLoader from '@/components/common/Table/TableLoader'
-import { selectValidator } from '@/actions/consensus'
-import { formatWei, formatWeiToNumber, addressShortener } from '@/utils/format'
-import { addDefaultSrc } from '@/utils/images'
-import WebsiteCell from './cells/website'
-import StakeAmountCell from './cells/stakeAmount'
+import React, { useEffect, useMemo } from "react";
+import { isMobile } from "react-device-detect";
+import { useSelector, useDispatch } from "react-redux";
+import { Formik, Form, useFormikContext } from "formik";
+import { object, string, boolean } from "yup";
+import map from "lodash/map";
+import filter from "lodash/filter";
+import sortBy from "lodash/sortBy";
+import Table from "@/components/common/Table";
+import TableHeader from "@/components/common/Table/TableHeader";
+import TableLoader from "@/components/common/Table/TableLoader";
+import { selectValidator } from "@/actions/consensus";
+import { formatWei, formatWeiToNumber, addressShortener } from "@/utils/format";
+import { addDefaultSrc } from "@/utils/images";
+import WebsiteCell from "./cells/website";
+import StakeAmountCell from "./cells/stakeAmount";
+// import images from "../../assets/validators/logos";
 
 const Shape = object().shape({
   validator: string(),
   showOnlyDelegators: boolean(),
   showOnlyOldNodes: boolean(),
-  showOnlyStaked: boolean()
-})
+  showOnlyStaked: boolean(),
+});
 
 const ValidatorsList = () => {
-  const { isLoading } = useSelector(state => state.consensus)
-  const entities = useSelector(state => state.entities.validators)
-  const { setFieldValue, submitForm, values } = useFormikContext()
-  const { showOnlyOldNodes, showOnlyDelegators, showOnlyStaked } = values
+  const { isLoading } = useSelector((state) => state.consensus);
+  const entities = useSelector((state) => state.entities.validators);
+  const { setFieldValue, submitForm, values } = useFormikContext();
+  const { showOnlyOldNodes, showOnlyDelegators, showOnlyStaked } = values;
   const data = useMemo(() => {
-    const rawData = showOnlyOldNodes
-      ? filter(entities, ['oldNode', true])
+    let rawData = showOnlyOldNodes
+      ? filter(entities, ["oldNode", true])
       : showOnlyDelegators && showOnlyStaked
-        ? filter(entities, ({ yourStake, forDelegation }) => !!Number(yourStake) && forDelegation)
-        : showOnlyDelegators
-          ? filter(entities, ['forDelegation', 1])
-          : showOnlyStaked
-            ? filter(entities, ({ yourStake }) => !!Number(yourStake))
-            : filter(entities, ['oldNode', false])
-
-    return sortBy(map(rawData, ({
-      yourStake,
-      name,
-      address,
-      fee,
-      delegatorsLength,
-      stakeAmount,
-      upTime,
-      forDelegation,
-      website,
-      oldNode
-    }) => ({
-      name: [
-        {
-          name: <div className='address'>{name || addressShortener(address)}</div>,
-          image: <img className='avatar' src={`${CONFIG.api.boot}/getNodeLogo=${address}`} onError={(e) => addDefaultSrc(e, address)} />
-        }
-      ],
-      address,
-      fee: isNaN(formatWeiToNumber(fee)) ? 0 : `${formatWei(fee) * 100} %`,
-      delegators: delegatorsLength,
-      upTime: `${upTime?.toString()?.substring(0, 4)} %`,
-      stakeAmount,
-      yourStake,
-      website,
-      isOpen: forDelegation && (formatWeiToNumber(fee) * 100) >= 15,
-      oldNode
-    })), [({ isOpen }) => !isOpen])
-  }, [entities, showOnlyDelegators, showOnlyStaked, showOnlyOldNodes])
+      ? filter(
+          entities,
+          ({ yourStake, forDelegation }) => !!Number(yourStake) && forDelegation
+        )
+      : showOnlyStaked
+      ? filter(entities, ({ yourStake }) => !!Number(yourStake))
+      : filter(entities, ["oldNode", false]);
+    rawData = rawData.map((validator) => {
+      const { image } = validator;
+      if(image){
+        const img = require(`@/assets/images/logos/${image.split("/")[1]}`).default;
+        return {
+          ...validator,
+          image: img,
+        };
+      }
+      return validator;
+    });
+    return sortBy(
+      map(
+        rawData,
+        ({
+          yourStake,
+          name,
+          image,
+          address,
+          fee,
+          delegatorsLength,
+          stakeAmount,
+          upTime,
+          forDelegation,
+          website,
+          oldNode,
+        }) => ({
+          name: [
+            {
+              name: (
+                <div className="address">
+                  {name || addressShortener(address)}
+                </div>
+              ),
+              image: (
+                <img
+                  className="avatar"
+                  src={image? image : ""}
+                  onError={(e) => addDefaultSrc(e, address)}
+                />
+              ),
+            },
+          ],
+          address,
+          fee: isNaN(formatWeiToNumber(fee)) ? 0 : `${formatWei(fee) * 100} %`,
+          delegators: delegatorsLength,
+          upTime: `${upTime?.toString()?.substring(0, 4)} %`,
+          stakeAmount,
+          yourStake,
+          website,
+          isOpen: formatWeiToNumber(fee) * 100 >= 15,
+          oldNode,
+        })
+      ),
+      [({ isOpen }) => !isOpen]
+    );
+  }, [entities, showOnlyDelegators, showOnlyStaked, showOnlyOldNodes]);
 
   const columns = useMemo(
-    () => isMobile
-      ? [{
-          accessor: 'name',
-          Header: <TableHeader header='name' tooltipText='The name of the validator.' id='name' />
-        },
-        {
-          accessor: 'stakeAmount',
-          Cell: StakeAmountCell,
-          Header: <TableHeader header='Staked amount' tooltipText='The amount of FUSE staked to each validator.' id='staked' />
-        },
-        {
-          accessor: 'fee',
-          Header: <TableHeader header='Fee' tooltipText='The % of the block rewards each validator takes.' id='fee' />
-        },
-        {
-          id: 'dropdown',
-          accessor: '',
-          Cell: ({ row: { original: { oldNode } } }) => <button className='button'><span>{oldNode ? 'Unstake' : 'Stake'}</span></button>
-        }]
-      : [
-          {
-            accessor: 'name',
-            Header: <TableHeader header='name' tooltipText='The name of the validator.' id='name' />
-          },
-          {
-            accessor: 'stakeAmount',
-            Cell: StakeAmountCell,
-            Header: <TableHeader header='Staked amount' tooltipText='The amount of FUSE staked to each validator.' id='staked' />
-          },
-          {
-            accessor: 'fee',
-            Header: <TableHeader header='Fee' tooltipText='The % of the block rewards each validator takes.' id='fee' />
-          },
-          {
-            accessor: 'upTime',
-            Header: <TableHeader header='Up time' tooltipText='The % of blocks filled by the validator over a 28 day rolling window.' id='upTime' />
-          },
-          {
-            accessor: 'website',
-            Header: <TableHeader header='website' id='website' />,
-            Cell: WebsiteCell
-          },
-          {
-            id: 'dropdown',
-            accessor: '',
-            Cell: ({ row: { original: { oldNode } } }) => <button className='button'><span>{oldNode ? 'Unstake' : 'Stake'}</span></button>
-          }], [isMobile])
+    () =>
+      isMobile
+        ? [
+            {
+              accessor: "name",
+              Header: (
+                <TableHeader
+                  header="name"
+                  tooltipText="The name of the validator."
+                  id="name"
+                />
+              ),
+            },
+            {
+              accessor: "stakeAmount",
+              Cell: StakeAmountCell,
+              Header: (
+                <TableHeader
+                  header="Staked amount"
+                  tooltipText="The amount of FUSE staked to each validator."
+                  id="staked"
+                />
+              ),
+            },
+            {
+              accessor: "fee",
+              Header: (
+                <TableHeader
+                  header="Fee"
+                  tooltipText="The % of the block rewards each validator takes."
+                  id="fee"
+                />
+              ),
+            },
+            {
+              id: "dropdown",
+              accessor: "",
+              Cell: ({
+                row: {
+                  original: { oldNode },
+                },
+              }) => (
+                <button className="button">
+                  <span>{oldNode ? "Unstake" : "Stake"}</span>
+                </button>
+              ),
+            },
+          ]
+        : [
+            {
+              accessor: "name",
+              Header: (
+                <TableHeader
+                  header="name"
+                  tooltipText="The name of the validator."
+                  id="name"
+                />
+              ),
+            },
+            {
+              accessor: "stakeAmount",
+              Cell: StakeAmountCell,
+              Header: (
+                <TableHeader
+                  header="Staked amount"
+                  tooltipText="The amount of FUSE staked to each validator."
+                  id="staked"
+                />
+              ),
+            },
+            {
+              accessor: "fee",
+              Header: (
+                <TableHeader
+                  header="Fee"
+                  tooltipText="The % of the block rewards each validator takes."
+                  id="fee"
+                />
+              ),
+            },
+            {
+              accessor: "upTime",
+              Header: (
+                <TableHeader
+                  header="Up time"
+                  tooltipText="The % of blocks filled by the validator over a 28 day rolling window."
+                  id="upTime"
+                />
+              ),
+            },
+            {
+              accessor: "website",
+              Header: <TableHeader header="website" id="website" />,
+              Cell: WebsiteCell,
+            },
+            {
+              id: "dropdown",
+              accessor: "",
+              Cell: ({
+                row: {
+                  original: { oldNode },
+                },
+              }) => (
+                <button className="button">
+                  <span>{oldNode ? "Unstake" : "Stake"}</span>
+                </button>
+              ),
+            },
+          ],
+    [isMobile]
+  );
 
-  const handleSelectValidator = ({ original: { address, isOpen, oldNode } }) => {
+  const handleSelectValidator = ({
+    original: { address, isOpen, oldNode },
+  }) => {
     if (isOpen || oldNode) {
-      setFieldValue('validator', address)
-      setTimeout(submitForm, 3)
+      setFieldValue("validator", address);
+      setTimeout(submitForm, 3);
     }
-  }
+  };
 
   const renderTable = () => {
     return (
@@ -133,36 +228,36 @@ const ValidatorsList = () => {
         count={Object.keys(entities).length}
         size={10}
       />
-    )
-  }
+    );
+  };
 
   return (
-    <div className='validator__list grid-y'>
+    <div className="validator__list grid-y">
       {isLoading ? <TableLoader /> : renderTable()}
     </div>
-  )
-}
+  );
+};
 
 const SelectValidatorForm = () => {
-  const dispatch = useDispatch()
-  const { validator } = useSelector(state => state.screens.stake)
+  const dispatch = useDispatch();
+  const { validator } = useSelector((state) => state.screens.stake);
   const onSubmit = (values, formikBag) => {
-    const { validator } = values
-    dispatch(selectValidator(validator))
-    formikBag.resetForm({ values })
-  }
+    const { validator } = values;
+    dispatch(selectValidator(validator));
+    formikBag.resetForm({ values });
+  };
 
   React.useEffect(() => {
     if (validator) {
-      dispatch(selectValidator(validator))
+      dispatch(selectValidator(validator));
     }
-  }, [])
+  }, []);
 
   return (
     <Formik
       initialValues={{
         validator,
-        showOnlyDelegators: true
+        showOnlyDelegators: true,
       }}
       onSubmit={onSubmit}
       validationSchema={Shape}
@@ -173,7 +268,7 @@ const SelectValidatorForm = () => {
         </Form>
       )}
     </Formik>
-  )
-}
+  );
+};
 
-export default SelectValidatorForm
+export default SelectValidatorForm;
--- src/sagas/consensus.js
+++ src/sagas/consensus.js
@@ -1,243 +1,256 @@
-import { all, call, put, takeEvery, select } from 'redux-saga/effects'
-import * as actions from '@/actions/consensus'
-import { tryTakeEvery } from './utils'
-import { getWeb3 } from '@/services/web3'
-import { transactionFlow } from './transaction'
+import { all, call, put, takeEvery, select } from "redux-saga/effects";
+import * as actions from "@/actions/consensus";
+import { tryTakeEvery } from "./utils";
+import { getWeb3 } from "@/services/web3";
+import { transactionFlow } from "./transaction";
 import {
   Consensus as ConsensusABI,
-  BlockReward as BlockRewardABI
-} from '@/constants/abi'
-import keyBy from 'lodash/keyBy'
-import omit from 'lodash/omit'
-import { balanceOfNative } from '@/actions/accounts'
-import { fetchNodeByAddress, fetchOldNodes } from '@/services/api/boot'
+  BlockReward as BlockRewardABI,
+} from "@/constants/abi";
+import validators from "../assets/validators/validators.json";
+import keyBy from "lodash/keyBy";
+import omit from "lodash/omit";
+import { balanceOfNative } from "@/actions/accounts";
+import { fetchNodeByAddress, fetchOldNodes } from "@/services/api/boot";
 
-function * getTotalStakeAmount () {
-  const web3 = yield getWeb3({ networkType: 'fuse' })
+function* getTotalStakeAmount() {
+  const web3 = yield getWeb3({ networkType: "fuse" });
   const consensusContract = new web3.eth.Contract(
     ConsensusABI,
     CONFIG.consensusAddress
-  )
+  );
   const totalStakeAmount = yield call(
     consensusContract.methods.totalStakeAmount().call
-  )
+  );
   yield put({
     type: actions.GET_TOTAL_STAKE_AMOUNT.SUCCESS,
     response: {
-      totalStakeAmount
-    }
-  })
+      totalStakeAmount,
+    },
+  });
 }
 
-function * getValidators () {
-  const web3 = yield getWeb3({ networkType: 'fuse' })
+function* getValidators() {
+  const web3 = yield getWeb3({ networkType: "fuse" });
   const consensusContract = new web3.eth.Contract(
     ConsensusABI,
     CONFIG.consensusAddress
-  )
-  const validators = yield call(consensusContract.methods.getValidators().call)
-  const entities = keyBy(validators, (address) => address)
+  );
+  const validators = yield call(consensusContract.methods.getValidators().call);
+  const entities = keyBy(validators, (address) => address);
   yield put({
     type: actions.GET_VALIDATORS.SUCCESS,
     response: {
       entities,
-      numberOfValidators: validators.length
-    }
-  })
+      numberOfValidators: validators.length,
+    },
+  });
 }
 
-function * getOldNodes () {
-  const { numberOfValidators = 0 } = yield select((state) => state.consensus)
-  const oldNodes = yield call(fetchOldNodes)
-  const validators = Object.keys(oldNodes)
+function* getOldNodes() {
+  const { numberOfValidators = 0 } = yield select((state) => state.consensus);
+  const oldNodes = yield call(fetchOldNodes);
+  const validators = Object.keys(oldNodes);
   for (const address in oldNodes) {
-    const validatorData = yield call(fetchValidatorData, { address })
+    const validatorData = yield call(fetchValidatorData, { address });
     yield put({
       type: actions.FETCH_VALIDATOR_METADATA.SUCCESS,
-      entity: 'validators',
+      entity: "validators",
       response: {
         address,
         ...validatorData,
-        ...omit(oldNodes[address], ['forDelegation']),
-        oldNode: true
-      }
-    })
+        ...omit(oldNodes[address], ["forDelegation"]),
+        oldNode: true,
+      },
+    });
   }
   yield put({
     type: actions.GET_VALIDATORS.SUCCESS,
     response: {
       entities: keyBy(validators, (address) => address),
       numberOfValidators: numberOfValidators + validators.length,
-      isOld: true
-    }
-  })
+      isOld: true,
+    },
+  });
 }
 
-function * fetchValidatorData ({ address }) {
-  const web3 = yield getWeb3({ networkType: 'fuse' })
+function* fetchValidatorData({ address }) {
+  const web3 = yield getWeb3({ networkType: "fuse" });
   const consensusContract = new web3.eth.Contract(
     ConsensusABI,
     CONFIG.consensusAddress
-  )
+  );
   const calls = {
     stakeAmount: call(consensusContract.methods.stakeAmount(address).call),
     fee: call(consensusContract.methods.validatorFee(address).call),
     delegatorsLength: call(
       consensusContract.methods.delegatorsLength(address).call
-    )
-  }
+    ),
+  };
 
-  const { accountAddress } = yield select((state) => state.network)
+  const { accountAddress } = yield select((state) => state.network);
   if (accountAddress) {
     calls.yourStake = call(
       consensusContract.methods.delegatedAmount(accountAddress, address).call
-    )
+    );
   }
 
-  return yield all(calls)
+  return yield all(calls);
 }
 
-function * fetchValidatorMetadata ({ address }) {
-  let response = { Node: { name: 'Anonymous Validator' } }
+function* fetchValidatorMetadata({ address }) {
+  let response = { Node: { name: "Anonymous Validator" } };
   try {
-    response = yield call(fetchNodeByAddress, { address })
+    response = yield call(fetchNodeByAddress, { address });
   } catch {
-    console.log('error fetching validator metadata')
+    console.log("error fetching validator metadata");
   }
-  const validatorData = yield call(fetchValidatorData, { address })
+  const validatorData = yield call(fetchValidatorData, { address });
   yield put({
     type: actions.FETCH_VALIDATOR_METADATA.SUCCESS,
-    entity: 'validators',
+    entity: "validators",
     response: {
       address,
       ...{
         ...response.Node,
         ...validatorData,
-        oldNode: false
-      }
-    }
-  })
+        name:
+          response.Node.name == "Anonymous Validator"
+            ? response.Node.name
+            : validators[address].name,
+        image:
+          response.Node.name == "Anonymous Validator"
+            ? ""
+            : validators[address].image,
+        website:
+          response.Node.name == "Anonymous Validator"
+            ? ""
+            : validators[address].website,
+        oldNode: false,
+      },
+    },
+  });
 }
 
-function * watchGetValidators ({ response: { entities, isOld } }) {
+function* watchGetValidators({ response: { entities, isOld } }) {
   if (isOld) {
-    return
+    return;
   }
   for (const validatorAddress in entities) {
-    yield put(actions.fetchValidatorMetadata(validatorAddress))
+    yield put(actions.fetchValidatorMetadata(validatorAddress));
   }
 }
 
-function * withdraw ({ validatorAddress, amount }) {
-  const { accountAddress } = yield select((state) => state.network)
+function* withdraw({ validatorAddress, amount }) {
+  const { accountAddress } = yield select((state) => state.network);
   if (accountAddress) {
-    const web3 = yield getWeb3()
+    const web3 = yield getWeb3();
     const consensusContract = new web3.eth.Contract(
       ConsensusABI,
       CONFIG.consensusAddress
-    )
+    );
     const data = yield consensusContract.methods
       .withdraw(validatorAddress, amount)
-      .encodeABI()
+      .encodeABI();
 
     const transactionObject = {
       from: accountAddress,
       to: CONFIG.consensusAddress,
       value: 0,
-      data
-    }
+      data,
+    };
 
-    const gasLimit = yield web3.eth.estimateGas(transactionObject)
+    const gasLimit = yield web3.eth.estimateGas(transactionObject);
 
     const transactionPromise = web3.eth.sendTransaction({
       ...transactionObject,
-      gasLimit
-    })
+      gasLimit,
+    });
 
-    const action = actions.WITHDRAW
-    yield call(transactionFlow, { transactionPromise, action })
+    const action = actions.WITHDRAW;
+    yield call(transactionFlow, { transactionPromise, action });
   }
 }
 
-function * delegate ({ validatorAddress, amount }) {
-  const { accountAddress } = yield select((state) => state.network)
+function* delegate({ validatorAddress, amount }) {
+  const { accountAddress } = yield select((state) => state.network);
   if (accountAddress) {
-    const web3 = yield getWeb3()
+    const web3 = yield getWeb3();
     const consensusContract = new web3.eth.Contract(
       ConsensusABI,
       CONFIG.consensusAddress
-    )
+    );
 
     const data = yield consensusContract.methods
       .delegate(validatorAddress)
-      .encodeABI()
+      .encodeABI();
 
     const transactionObject = {
       from: accountAddress,
       to: CONFIG.consensusAddress,
       value: amount,
-      data
-    }
+      data,
+    };
 
-    const gasLimit = yield web3.eth.estimateGas(transactionObject)
+    const gasLimit = yield web3.eth.estimateGas(transactionObject);
 
     const transactionPromise = web3.eth.sendTransaction({
       ...transactionObject,
-      gasLimit
-    })
-    const action = actions.DELEGATE
-    yield call(transactionFlow, { transactionPromise, action })
+      gasLimit,
+    });
+    const action = actions.DELEGATE;
+    yield call(transactionFlow, { transactionPromise, action });
   }
 }
 
-function * getBlockRewardAmount () {
-  const web3 = yield getWeb3({ networkType: 'fuse' })
+function* getBlockRewardAmount() {
+  const web3 = yield getWeb3({ networkType: "fuse" });
   const blockRewardContract = new web3.eth.Contract(
     BlockRewardABI,
     CONFIG.blockRewardAddress
-  )
+  );
   const rewardPerBlock = yield call(
     blockRewardContract.methods.getBlockRewardAmount().call
-  )
+  );
   yield put({
     type: actions.GET_BLOCK_REWARD_AMOUNT.SUCCESS,
     response: {
-      rewardPerBlock
-    }
-  })
+      rewardPerBlock,
+    },
+  });
 }
 
-function * getBlockRewardAmountPerValidator ({ address }) {
-  const web3 = yield getWeb3({ networkType: 'fuse' })
+function* getBlockRewardAmountPerValidator({ address }) {
+  const web3 = yield getWeb3({ networkType: "fuse" });
   const blockRewardContract = new web3.eth.Contract(
     BlockRewardABI,
     CONFIG.blockRewardAddress
-  )
+  );
   const rewardPerYourBlock = yield call(
     blockRewardContract.methods.getBlockRewardAmountPerValidator(address).call
-  )
+  );
   yield put({
     type: actions.GET_BLOCK_REWARD_AMOUNT_PER_VALIDATOR.SUCCESS,
-    entity: 'validators',
+    entity: "validators",
     response: {
       address,
-      rewardPerYourBlock
-    }
-  })
+      rewardPerYourBlock,
+    },
+  });
 }
 
-function * watchStakingSuccess () {
-  const { accountAddress } = yield select((state) => state.network)
-  const { validator } = yield select((state) => state.screens.stake)
+function* watchStakingSuccess() {
+  const { accountAddress } = yield select((state) => state.network);
+  const { validator } = yield select((state) => state.screens.stake);
   if (accountAddress) {
-    yield put(balanceOfNative(accountAddress))
-    yield put(actions.getTotalStakeAmount())
-    yield put(actions.getValidators())
-    yield put(actions.fetchValidatorMetadata(validator))
+    yield put(balanceOfNative(accountAddress));
+    yield put(actions.getTotalStakeAmount());
+    yield put(actions.getValidators());
+    yield put(actions.fetchValidatorMetadata(validator));
   }
 }
 
-export default function * consensusSaga () {
+export default function* consensusSaga() {
   yield all([
     tryTakeEvery(actions.WITHDRAW, withdraw, 1),
     tryTakeEvery(actions.DELEGATE, delegate, 1),
@@ -255,6 +268,6 @@ export default function * consensusSaga () {
     takeEvery(
       [actions.WITHDRAW.SUCCESS, actions.DELEGATE.SUCCESS],
       watchStakingSuccess
-    )
-  ])
+    ),
+  ]);
 }
